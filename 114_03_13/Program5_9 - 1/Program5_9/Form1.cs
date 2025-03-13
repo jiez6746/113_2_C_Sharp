34e5d2cc2c27dd5657e2081d381b5289c5d9071e
jiez6746 <A113221038@mail.shu.edu.tw>
2025-03-13 12:24:55 +0800

    114_03_13

---
+++ 114_03_13/Program5_9 - 1/Program5_9/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program5_9
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random(); // Create a random object
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           int n1 = random.Next(1, 7); //產生1-7的隨機變數，有效範圍在button1_Click中
+           int n2 = random.Next(1, 7); //產生1-7的隨機變數，有效範圍在button1_Click中
+
+           showpicturebox(n1, pictureBox1 ); //呼叫showpicturebox方法
+
+           showpicturebox(n2 ,pictureBox2 ); //呼叫showpicturebox方法
+
+
+
+
+        }
+
+            private void showpicturebox(int n, PictureBox pic)
+         {
+            switch (n)
+            {
+                case 1:
+                    pic.Image = Properties.Resources.Die1;
+                    break;
+                case 2:
+                    pic.Image = Properties.Resources.Die2;
+                    break;
+                case 3:
+                    pic.Image = Properties.Resources.Die3;
+                    break;
+                case 4:
+                    pic.Image = Properties.Resources.Die4;
+                    break;
+                case 5:
+                    pic.Image = Properties.Resources.Die5;
+                    break;
+                case 6:
+                    pic.Image = Properties.Resources.Die6;
+                    break;
+
+            }
+        }
+
+
+
+
+
+    }
+}
--- 114_03_13/Program5_9/Program5_9/Form1.cs
+++ 114_03_13/Program5_9/Program5_9/Form1.cs
@@ -11,6 +11,7 @@ namespace Program5_9
 {
     public partial class Form1 : Form
     {
+        Random random = new Random(); // Create a random object
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +21,64 @@ namespace Program5_9
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
-        }
+           int n1 = random.Next(1, 7); //產生1-7的隨機變數，有效範圍在button1_Click中
+           int n2 = random.Next(1, 7); //產生1-7的隨機變數，有效範圍在button1_Click中
+
+            switch (n1) 
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Die1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Die2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Die3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Die4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Die5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Die6;
+                    break;
+
+            }
+
+            switch (n2)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.Die1;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.Die2;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.Die3;
+                    break;
+                case 4:
+                    pictureBox2.Image = Properties.Resources.Die4;
+                    break;
+                case 5:
+                    pictureBox2.Image = Properties.Resources.Die5;
+                    break;
+                case 6:
+                    pictureBox2.Image = Properties.Resources.Die6;
+                    break;
+
+
+
+                     
+
+
+
+
+
+            }
+
+
+            }
     }
 }
