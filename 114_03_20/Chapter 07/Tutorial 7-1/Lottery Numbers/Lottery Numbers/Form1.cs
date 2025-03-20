df34dd7b018eddd1e03061eeaf1a2aa082472c13
jiez6746 <A113221038@mail.shu.edu.tw>
2025-03-20 12:55:32 +0800

    114_03_20

--- 114_03_20/Chapter 07/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
+++ 114_03_20/Chapter 07/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -19,12 +19,38 @@ namespace Lottery_Numbers
 
         private void generateButton_Click(object sender, EventArgs e)
         {
-           
+            const int SIZE = 5; // 陣列的大小
+            int[] lotteryNumbers = new int[SIZE];//儲存樂透號碼
+            Random rand = new Random();
+
+           for (int i = 0;i <lotteryNumbers.Length; i++)
+            {
+                lotteryNumbers[i] = rand.Next(1, 43);// 產生1~42的亂數
+            }
+
+            //firstLabel.Text = lotteryNumbers[0].ToString();
+            //secondLabel.Text = lotteryNumbers[1].ToString();
+            //thirdLabel.Text = lotteryNumbers[2].ToString();
+            //fourthLabel.Text = lotteryNumbers[3].ToString();
+            //fifthLabel.Text = lotteryNumbers[4].ToString();
+
+            // 顯示樂透號碼
+
+            Label[] showlabels = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+            for(int i =0;i<lotteryNumbers.Length; i++)
+            {
+                showlabels[i].Text = lotteryNumbers[i].ToString();
+            }
+
+
+
+
+
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            // Close the form.
+            // 關閉表單
             this.Close();
         }
     }
